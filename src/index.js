855cbea4763a17befe42f75aefe4827bb1c4df46
EA <altmanea@gmail.com>
2023-06-19 15:06:43 +0600

    remove plugin submodule

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import Deck, { VERSION } from '../reveal.js/js/reveal';
 import '../reveal.js/dist/reveal.css';
 import '../reveal.js/dist/theme/white.css';
 import RevealMarkdown from '../reveal.js/plugin/markdown/markdown'
+import '../reveal.js-plugins/audio-slideshow/plugin';
+import '../reveal.js-plugins/audio-slideshow/recorder';
+import '../reveal.js-plugins/audio-slideshow/RecordRTC';
 
 window.slide_init = function slide_init() {
   const deck = new Deck({
-    plugins: [RevealMarkdown]
+    embedded: true,
+    plugins: [RevealMarkdown, RevealAudioSlideshow, RevealAudioRecorder],
+    // audio: {
+    //   prefix: filename + '/',
+    //   suffix: '.mp3',
+    // },
   });
   deck.initialize();
   return deck;
 }
 
+slide_init();
 
